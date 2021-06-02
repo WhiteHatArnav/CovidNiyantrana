e712ecda81b4c9a36e3a2309d47258f5bbc38cc6
Arnav Joshi <arnavjsh12@gmail.com>
2021-06-02 12:14:16 -0400

    Correcting Labeling Errors and Data Update

--- IndiaVaxProdAnalysis.py
+++ IndiaVaxProdAnalysis.py
@@ -6,7 +6,7 @@ from sklearn.metrics import r2_score
 TotalPop = 1360000000
 HerdImmPop = 0.75  * TotalPop 
 
-Jabs = 165000000
+Jabs = 215000000
 CovidRecovered = 300000000
 TargetJabs = HerdImmPop*2 - CovidRecovered
 ToBeJabbed = TargetJabs - Jabs
@@ -22,9 +22,9 @@ CovShProdTarget = 65000000
 OtherVaxTarget = 5000000
 
 MonthlyVax = CovaxProd + CovShProd + SputnikProd
-TargetMonthlyVax = CovaxProdTarget + CovShProdTarget + OtherVaxTarget
+TargetMonthlyVax = 300000000
 
-
+TargetJabs2 = 2000000000
 
 y = np.arange(Jabs,TargetJabs, MonthlyVax)
 n = len(y)
@@ -32,10 +32,14 @@ x = range(1,n+1)
 
 
 
-
-y2 = np.arange(y[3],TargetJabs, TargetMonthlyVax)
+y2 = np.arange(y[2],TargetJabs2, TargetMonthlyVax)
 n2 = len(y2)
-x2 = range(4,n2+4)
+x2 = range(3,n2+3)
+
+y3 = np.arange(y[3],TargetJabs, TargetMonthlyVax)
+n3 = len(y2)
+x3 = range(4,n2+4)
+
 
 
 targLine = [y[n-1]] * 26
@@ -51,12 +55,13 @@ TargetRateLabel = str(TargetMonthlyVax/1000000) + ' Mil.'
 plt.scatter(x,y)
 plt.scatter(x2, y2, color = 'green')
 
-for i in x:
-    j = y[i-1]
+for i in range(0, n):
+    j = y[i]
+    
     if(i < 12):
-        month  = MonthRef[i-1] 
+        month  = MonthRef[i] 
     if(i >= 12): 
-        month = MonthRef[(i -12-1)]
+        month = MonthRef[(i -12)]
 
     if(i <= 7):
         xlab =  month + ' ' + YearRef[0]
@@ -68,18 +73,16 @@ for i in x:
     yinMil = j/1000000
     ylab =  str(yinMil) + ' Mil.'
     
-    if(i == 1 or i%3 == 0 or i == n):
-        if(i != n-1):
-            plt.annotate( '('+ xlab + ', ' + ylab +')', (i+1,j), color = 'blue')
+    if(i == 0 or i%3 == 0 or i == n-1):
+        plt.annotate( '('+ xlab + ', ' + ylab +')', (i+1.5,j-25000000), color = 'blue')
 
 
-
-for i in x2:
-    j = y2[i-4]
+for i in range(0,n2):
+    j = y2[i]
     if(i < 12):
-        month  = MonthRef[i-1] 
+        month  = MonthRef[i+2] 
     if(i >= 12): 
-        month = MonthRef[(i -12-1)]
+        month = MonthRef[(i -12+2)]
 
     if(i <= 7):
         xlab =  month + ' ' + YearRef[0]
@@ -90,10 +93,8 @@ for i in x2:
 
     yinMil = j/1000000
     ylab =  str(yinMil) + ' Mil.'
-    
-    if(i == 2 or i%2 == 0  or i == n2+3):
-        if(i != n2+2):
-            plt.annotate( '('+ xlab + ', ' + ylab +')', (i-4,j), color = 'green')
+    print(j)
+    plt.annotate( '('+ xlab + ', ' + ylab +')', (i-1.25,j-25000000), color = 'green')
 
 plt.title('Vaccination Trend in India at Current and Government Target Rate')
 plt.plot(x,y, label = 'Total Vaccine jabs at Current Rate of: ' + CurRateLabel + " Per Month")
@@ -101,8 +102,9 @@ plt.plot(x2, y2, color = 'green', label = 'Total Vaccine Jabs at Target Rate of:
 plt.plot(targLine, color = 'red')
 plt.xlabel('Number of Months from May 2020')
 plt.ylabel('Total number of Covid 19 Vaccination Shots Administered in India')
-plt.annotate('Target No.  of  Vaccine shots: ' + str(y[n-1]/1000000) + ' Mil.', (2, y[n-1]+10000000), color = 'red')
-plt.xlim(1,25)
+plt.annotate('Target No.  of  Vaccine shots: ' + str(y[n-1]/1000000) + ' Mil.', (8, y[n-1]+10000000), color = 'red')
+plt.xlim(0,25)
+plt.ylim(125000000,1900000000)
 plt.legend()
 plt.show()
 
